cb589d96b0bc3d3c73073fbe176dd3d24c2843d5
med-amine01 <m.chebbi.tech@gmail.com>
2024-02-03 15:12:02 +0100

    good open close pattern

--- open-close/src/main/java/com/chebbi/tech/CallHistory.java
+++ open-close/src/main/java/com/chebbi/tech/CallHistory.java
@@ -1,18 +1,34 @@
 package com.chebbi.tech;
 
 import java.time.LocalDateTime;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class CallHistory {
 
+    private static final Map<Long, List<Call>> CALLS = new HashMap<>();
+
+    public synchronized static List<Call> getCurrentCalls(Long subscriberId) {
+        if (!CALLS.containsKey(subscriberId)) {
+            return Collections.emptyList();
+        }
+        return CALLS.get(subscriberId);
+    }
+
+    public synchronized static void addSession(Long subscriberId, LocalDateTime begin, long duration) {
+        List<Call> calls;
+        if (!CALLS.containsKey(subscriberId)) {
+            calls = new LinkedList<>();
+            CALLS.put(subscriberId, calls);
+        } else {
+            calls = CALLS.get(subscriberId);
+        }
+        calls.add(new Call(subscriberId, begin, duration));
+    }
+
     public static class Call {
-        
+
         private LocalDateTime begin;
-        
+
         private long duration;
 
         private Long subscriberId;
@@ -44,25 +60,5 @@ public class CallHistory {
             return subscriberId;
         }
 
-    }
-
-    private static final Map<Long, List<Call>> CALLS = new HashMap<>();
-
-    public synchronized static List<Call> getCurrentCalls(Long subscriberId) {
-        if(!CALLS.containsKey(subscriberId)) {
-            return Collections.emptyList();
-        }
-        return CALLS.get(subscriberId);
-    }
-
-    public synchronized static void addSession(Long subscriberId, LocalDateTime begin, long duration) {
-        List<Call> calls;
-        if(!CALLS.containsKey(subscriberId)) {
-            calls = new LinkedList<>();
-            CALLS.put(subscriberId, calls);
-        } else {
-            calls = CALLS.get(subscriberId);
-        }
-        calls.add(new Call(subscriberId, begin, duration));
     }
 }
