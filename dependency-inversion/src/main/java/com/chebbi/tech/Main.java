d56f342e4283c0272873633e759d876f0db27f7e
med-amine01 <m.chebbi.tech@gmail.com>
2024-02-03 18:25:47 +0100

    bad dependency inversion pattern

---
+++ dependency-inversion/src/main/java/com/chebbi/tech/Main.java
@@ -0,0 +1,12 @@
+package com.chebbi.tech;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        Message msg = new Message("This is a message again");
+        MessagePrinter printer = new MessagePrinter();
+        printer.writeMessage(msg, "test_msg.txt");
+    }
+}
